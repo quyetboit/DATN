450318978eef928c69071344d496c8674b98a116
quyetboit <86332668+quyetboit@users.noreply.github.com>
2021-12-10 08:02:47 +0700

    add file connect db

--- model/php/db.php
+++ model/php/db.php
@@ -1,3 +1,13 @@
 <?php
+    $serverName = "localhost";
+    $userName = "root";
+    $password = "quyetboit";
+    $databaseName = "ql_nhac";
 
-    
+    $conn = new mysqli($serverName, $userName, $password, $databaseName);
+
+    if($conn->connect_error) {
+        die("Kết nối không thành công" . $conn -> connect_error);
+    } else {
+        echo "Connect seccess!";
+    }
